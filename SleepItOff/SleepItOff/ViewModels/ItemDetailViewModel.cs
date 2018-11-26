905ec14f18bb3e5f5787f0e4cdff9d9df8ca409d
grisha <grishal012@gmail.com>
2018-11-26 10:26:57 +0200

    Add project files.

---
+++ SleepItOff/SleepItOff/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SleepItOff.Models;
+
+namespace SleepItOff.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
